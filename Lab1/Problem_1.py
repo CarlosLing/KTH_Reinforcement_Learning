cc84028fd2652ba12a7f57adc832d6a2b8bfc4c0
CarlosLing <37625533+CarlosLing@users.noreply.github.com>
2019-11-19 17:08:39 +0100

    Lab1P1

---
+++ Lab1/Problem_1.py
@@ -0,0 +1,76 @@
+import numpy as np
+from Lab1.maze import generate_maze, fill_values
+
+
+if __name__ == '__main__':
+
+    xdim = 7
+    ydim = 8
+    Tmax = 20
+    dimensions = [xdim, ydim]
+    walls = [(0, 2), (1, 2), (2, 2), (3, 2), (1, 5), (2, 5), (3, 5), (2, 6), (2, 7), (5, 1), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (6, 4)]
+    maze = generate_maze(dimensions, walls)
+    maze_mino = generate_maze(dimensions, [])
+    escape_pos = [6,5]
+
+    values = -np.ones((xdim,ydim,xdim,ydim,Tmax))
+    actions_op = -np.ones((xdim,ydim,xdim,ydim,Tmax))
+
+    actions = [[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0]]
+    actions_mino = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+    # Fill in initial states
+    for x_per in range(xdim):
+        for y_per in range(ydim):
+            for x_min in range(xdim):
+                for y_min in range(ydim):
+                    # Do not complete imposible settings i.e. person in the wall
+                    if maze[x_per+1, y_per+1] and maze_mino[x_min+1,y_min+1]:
+                        if ([x_per, y_per] == escape_pos) and ([x_min, y_min] != escape_pos):
+                            values[x_per, y_per, x_min, y_min, Tmax-1] = 1
+                        else:
+                            values[x_per, y_per, x_min, y_min, Tmax - 1] = 0
+
+    for t in range(Tmax-1):
+        for x_per in range(xdim):
+            for y_per in range(ydim):
+                for x_min in range(xdim):
+                    for y_min in range(ydim):
+                        v, a = fill_values(x_per, y_per, x_min, y_min, t,
+                                           Tmax, maze, maze_mino, values, escape_pos, actions, actions_mino)
+                        values[x_per, y_per, x_min, y_min, Tmax - 2 - t] = v
+                        actions_op[x_per, y_per, x_min, y_min, Tmax - 2 - t] = a
+
+    # Vestion 2: Minotaur can stay still ################
+
+    values_2 = -np.ones((xdim, ydim, xdim, ydim, Tmax))
+    actions_op_2 = -np.ones((xdim, ydim, xdim, ydim, Tmax))
+
+    actions = [[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0]]
+    actions_mino = [[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0]]
+
+    # Fill in initial states
+    for x_per in range(xdim):
+        for y_per in range(ydim):
+            for x_min in range(xdim):
+                for y_min in range(ydim):
+                    # Do not complete imposible settings i.e. person in the wall
+                    if maze[x_per + 1, y_per + 1] and maze_mino[x_min + 1, y_min + 1]:
+                        if ([x_per, y_per] == escape_pos) and ([x_min, y_min] != escape_pos):
+                            values_2[x_per, y_per, x_min, y_min, Tmax - 1] = 1
+                        else:
+                            values_2[x_per, y_per, x_min, y_min, Tmax - 1] = 0
+
+    for t in range(Tmax - 1):
+        for x_per in range(xdim):
+            for y_per in range(ydim):
+                for x_min in range(xdim):
+                    for y_min in range(ydim):
+                        v, a = fill_values(x_per, y_per, x_min, y_min, t,
+                                           Tmax, maze, maze_mino, values_2, escape_pos, actions, actions_mino)
+                        values_2[x_per, y_per, x_min, y_min, Tmax - 2 - t] = v
+                        actions_op_2[x_per, y_per, x_min, y_min, Tmax - 2 - t] = a
+
+    test1 = values[:, :, 6, 5, 4]
+
+
---
+++ Lab1/maze.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def generate_maze(shape, walls):
+    """ The function generates a maze that will have dimension , shape, ans walls:  """
+    maze = np.ones(shape)
+
+    for i in walls:
+        maze[i] = 0
+
+    return np.pad(maze, 1, mode='constant')
+
+
+def fill_values(x_per, y_per, x_min, y_min, t, Tmax, maze, maze_mino, values, escape_pos, actions, actions_mino):
+    max_value = -1
+    optimal_action = -1
+
+    if maze[x_per + 1, y_per + 1] and maze_mino[x_min + 1, y_min + 1]:
+        # if Escapes value = 1
+        if ([x_per, y_per] == escape_pos) and ([x_min, y_min] != escape_pos):
+            max_value = 1
+        elif [x_per, y_per] == [x_min, y_min]:
+            max_value = 0
+        else:
+            a = 0
+            # iterate on actions for humans
+            for a_per in actions:
+                value = 0
+                if maze[x_per + 1 + a_per[0], y_per + 1 + a_per[1]]:
+                    sum_a_mino = 0
+                    for a_mino in actions_mino:
+                        if maze_mino[x_min + 1 + a_mino[0], y_min + 1 + a_mino[1]]:
+                            value = value + values[
+                                x_per + a_per[0], y_per + a_per[1], x_min + a_mino[0], y_min + a_mino[1], Tmax - 1 - t]
+                            sum_a_mino = sum_a_mino + maze_mino[x_min + 1 + a_mino[0], y_min + 1 + a_mino[1]]
+                    value = value / sum_a_mino
+                else:
+                    value = -10
+                # We will choose the best action and save a code
+                if max_value < value:
+                    max_value = value
+                    optimal_action = a
+
+                a = a + 1
+
+    return max_value, optimal_action
+
+
